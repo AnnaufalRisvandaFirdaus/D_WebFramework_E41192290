1a2645b1dfb31253b8fbd24a7f760b5993530cbf
AnnaufalRisvandaFirdaus <annaufalfir1@gmail.com>
2021-04-01 12:29:21 +0700

    laravel 6.0

---
+++ laravel/app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManagementUserController extends Controller
+{
+    publiC function index(){
+        return "Halo nama saya Annaufal Risvanda Firdaus E41192290 TIF Golongan D 2019.";
+    }
+    //disini controller Management User
+}
+
+
+?>
